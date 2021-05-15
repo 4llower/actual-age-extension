f724332ed4300e2870184e8a0feb29c5805cc67b
4llower <95350098@study.bsuir.by>
2021-05-16 02:25:59 +0300

    add: newtab & merge with popup

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -8,9 +8,10 @@ const PATHS = require('./paths');
 // Merge webpack configuration files
 const config = merge(common, {
   entry: {
+    app: PATHS.src + '/app.js',
+    background: PATHS.src + '/background.js',
     popup: PATHS.src + '/popup.js',
     contentScript: PATHS.src + '/contentScript.js',
-    background: PATHS.src + '/background.js',
   },
 });
 
--- src/popup.js
+++ src/popup.js
@@ -3,13 +3,6 @@
 import './popup.css';
 
 (function() {
-  // We will make use of Storage API to get and store `count` value
-  // More information on Storage API can we found at
-  // https://developer.chrome.com/extensions/storage
-
-  // To get storage access, we have to mention it in `permissions` property of manifest.json file
-  // More information on Permissions can we found at
-  // https://developer.chrome.com/extensions/declare_permissions
   const counterStorage = {
     get: cb => {
       chrome.storage.sync.get(['count'], result => {
