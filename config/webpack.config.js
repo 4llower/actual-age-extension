c758743e77cce7551c2febca04ccf38a0a5f03b3
4llower <95350098@study.bsuir.by>
2021-05-16 02:40:36 +0300

    fix: naming

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -8,7 +8,7 @@ const PATHS = require('./paths');
 // Merge webpack configuration files
 const config = merge(common, {
   entry: {
-    app: PATHS.src + '/apps/tab/app.js',
+    tab: PATHS.src + '/apps/tab/tab.js',
     background: PATHS.src + '/apps/background/background.js',
     popup: PATHS.src + '/apps/popup/popup.js',
     contentScript: PATHS.src + '/apps/popup/contentScript.js',
