3661524e2d12e878bc324eb9f7841849fe63ee6e
4llower <95350098@study.bsuir.by>
2021-05-16 22:22:01 +0300

    add: smooth

--- src/apps/tab/tab.js
+++ src/apps/tab/tab.js
@@ -1,7 +1,18 @@
 'use strict'
 
 import './styles'
+import moment from 'moment'
+
+const POLLING_TIMEOUT = 333
+const FIXED_POINT = 8
+const MINIMAL_EXTRA_VALUE = Math.pow(1 / 10, FIXED_POINT)
+
 ;(function () {
+  const loader = document.getElementById('loader')
+  const age = document.getElementById('age')
+  const ageMain = document.getElementById('age__main')
+  const ageExtra = document.getElementById('age__extra')
+
   const storage = {
     get: (name, cb) => {
       chrome.storage.sync.get([name], (result) => {
@@ -20,12 +31,69 @@ import './styles'
     },
   }
 
+  const getMain = (birthday) => {
+    const date = moment(birthday)
+    return moment().diff(date, 'years')
+  }
+
+  const formatExtra = (extra) => extra.toFixed(FIXED_POINT).slice(1)
+
+  const getExtra = (birthday) => {
+    const main = getMain(birthday)
+    const seconds = moment(birthday)
+      .add(main + 1, 'years')
+      .diff(moment(birthday).add(main, 'years'), 'seconds')
+
+    const diff = moment(birthday)
+      .add(main + 1, 'years')
+      .diff(moment(), 'seconds')
+
+    return formatExtra(1 - diff / seconds)
+  }
+
+  let isStarted = false
+  let interval
+
+  const setup = (birthday) => {
+    isStarted = true
+    loader.style.display = 'none'
+    age.style.display = 'flex'
+
+    if (interval) clearInterval(interval)
+
+    let lastExtra
+    let smoothExtra
+
+    interval = setInterval(() => {
+      ageMain.innerText = getMain(birthday)
+      const extra = getExtra(birthday)
+      if (!lastExtra) {
+        lastExtra = extra
+        ageExtra.innerText = getExtra(birthday)
+      } else {
+        if (extra !== lastExtra) {
+          ageExtra.innerText = extra
+          smoothExtra = undefined
+          lastExtra = extra
+        } else {
+          if (!smoothExtra) {
+            smoothExtra = parseFloat(lastExtra)
+          }
+          smoothExtra += MINIMAL_EXTRA_VALUE
+          ageExtra.innerText = formatExtra(smoothExtra)
+        }
+      }
+    }, POLLING_TIMEOUT)
+  }
+
   chrome.runtime.onMessage.addListener(({ type, payload }) => {
     if (type === 'UPDATE_BIRTHDAY') {
-      console.log(payload)
+      setup(payload.birthday)
     }
     return true
   })
 
-  storage.get('birthday', console.log)
+  storage.get('birthday', (birthday) => {
+    if (!isStarted) setup(birthday)
+  })
 })()
